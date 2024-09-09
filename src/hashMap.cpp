86404dd26db3d89de248f1f9ee36654f1b8edab2
ersek <ersek.norbert@simplesoft.hu>
2024-09-09 15:06:39 +0200

    add hashMap.h and hashMap.cpp

---
+++ src/hashMap.cpp
@@ -0,0 +1,35 @@
+//
+// Created by hajlektalan on 2024-09-03.
+//
+
+#include <vector>
+#include <unordered_map>
+#include "header/hashMap.h"
+#include "header/commons.h"
+
+
+std::vector<WordInfo> hashMap::compressHashMap(const std::basic_string<char> &INPUT) {
+    std::vector<std::basic_string<char> > raw_data;
+
+    Commons::split(INPUT, raw_data, ' ');
+
+    std::unordered_map<std::basic_string<char>, int> wordCodeMap;
+    std::vector<WordInfo> encodedData;
+
+    int currentCode = 1;
+    std::unordered_map<std::basic_string<char>, int> firstOccurrence;
+
+    for (int i = 0; i < raw_data.size(); ++i) {
+        const std::basic_string<char> &word = raw_data[i];
+        if (firstOccurrence.find(word) == firstOccurrence.end()) {
+            firstOccurrence[word] = i;
+            wordCodeMap[word] = currentCode++;
+
+            encodedData.push_back({wordCodeMap[word], 0});
+        } else {
+            int distance = i - firstOccurrence[word];
+            encodedData.push_back({wordCodeMap[word], distance});
+        }
+    }
+    return encodedData;
+}
---
+++ src/header/hashMap.h
@@ -0,0 +1,20 @@
+//
+// Created by hajlektalan on 2024-09-03.
+//
+
+#ifndef HASHMAP_H
+#define HASHMAP_H
+
+#include <string>
+
+struct WordInfo { // this can definitely be done without this
+    int code;
+    int distance;
+};
+
+class hashMap {
+public:
+    static std::vector<WordInfo> compressHashMap(const std::basic_string<char> &INPUT);
+};
+
+#endif //HASHMAP_H
