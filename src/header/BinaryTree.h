25415cc778ce94fdd2b49ad68e36e3678a6a88fb
ersek <ersek.norbert@simplesoft.hu>
2024-09-15 15:38:35 +0200

    format file

--- src/header/BinaryTree.h
+++ src/header/BinaryTree.h
@@ -4,106 +4,96 @@
 #include <iostream>
 #include <queue>
 
-template <typename T>
-class Node
-{
+template<typename T>
+class Node {
 public:
     T data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
     explicit Node(T value);
 };
 
-template <typename T>
-class BinaryTree
-{
+template<typename T>
+class BinaryTree {
 public:
     BinaryTree();
+
     void insertNode(T value);
+
     void deleteNode(T value);
+
     bool search(T value);
+
     void inorder();
+
     void preorder();
+
     void postorder();
+
     void levelOrder();
 
 private:
-    Node<T>* root;
+    Node<T> *root;
 
-    Node<T>* _deleteRecursive(Node<T>* current, T value)
-    {
+    Node<T> *_deleteRecursive(Node<T> *current, T value) {
         if (current == nullptr) return nullptr;
 
-        if (current->data == value)
-        {
-            if (current->left == nullptr && current->right == nullptr)
-            {
+        if (current->data == value) {
+            if (current->left == nullptr && current->right == nullptr) {
                 delete current;
                 return nullptr;
             }
-            if (current->left == nullptr)
-            {
-                Node<T>* temp = current->right;
+            if (current->left == nullptr) {
+                Node<T> *temp = current->right;
                 delete current;
                 return temp;
             }
-            if (current->right == nullptr)
-            {
-                Node<T>* temp = current->left;
+            if (current->right == nullptr) {
+                Node<T> *temp = current->left;
                 delete current;
                 return temp;
             }
 
-            Node<T>* successor = _findMin(current->right);
+            Node<T> *successor = _findMin(current->right);
             current->data = successor->data;
             current->right = _deleteRecursive(current->right, successor->data);
-        }
-        else
-        {
+        } else {
             current->left = _deleteRecursive(current->left, value);
             current->right = _deleteRecursive(current->right, value);
         }
         return current;
     }
 
-    Node<T>* _findMin(Node<T>* node)
-    {
+    Node<T> *_findMin(Node<T> *node) {
         while (node->left != nullptr) node = node->left;
         return node;
     }
 
-    bool _searchRecursive(Node<T>* current, T value)
-    {
+    bool _searchRecursive(Node<T> *current, T value) {
         if (current == nullptr) return false;
         if (current->data == value) return true;
         return _searchRecursive(current->left, value) || _searchRecursive(current->right, value);
     }
 
-    void _inorderRecursive(Node<T>* node)
-    {
-        if (node != nullptr)
-        {
+    void _inorderRecursive(Node<T> *node) {
+        if (node != nullptr) {
             _inorderRecursive(node->left);
             std::cout << node->data << " ";
             _inorderRecursive(node->right);
         }
     }
 
-    void _preorderRecursive(Node<T>* node)
-    {
-        if (node != nullptr)
-        {
+    void _preorderRecursive(Node<T> *node) {
+        if (node != nullptr) {
             std::cout << node->data << " ";
             _preorderRecursive(node->left);
             _preorderRecursive(node->right);
         }
     }
 
-    void _postorderRecursive(Node<T>* node)
-    {
-        if (node != nullptr)
-        {
+    void _postorderRecursive(Node<T> *node) {
+        if (node != nullptr) {
             _postorderRecursive(node->left);
             _postorderRecursive(node->right);
             std::cout << node->data << " ";
@@ -111,6 +101,6 @@ private:
     }
 };
 
-#include "../BinaryTree.tpp"  // Include the implementation file for templates
+#include "../BinaryTree.tpp" //template implementation
 
 #endif  // BINARYTREE_H
