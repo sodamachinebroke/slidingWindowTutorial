fdbf074933f0dd81f56d73b1881b5a981f4e2e00
ersek <ersek.norbert@simplesoft.hu>
2024-09-18 18:12:21 +0200

    add [NOT WORKING] testing new algorithms

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <fstream>
 
-#include "src/header/BinaryTree.h"
+#include "src/BinaryTree.h"
+#include "src/BitReader.h"
 
 
 int main() {
 
     std::string INPUT = "on a rainy day the rain falls softly the rain falls softly and the world is wet the wet world reflects the rain the rain falls softly softly on the wet ground the ground is wet because the rain falls softly the rain falls gently gently on the wet streets the streets are wet and the rain continues the rain continues softly softly and gently gently the rain falls falls softly on the trees the trees are wet from the rain the rain falls softly and gently softly and gently the rain continues to fall the world is wet and the rain is gentle the rain falls gently softly and the wet world reflects the gentle rain";
-    /*std::vector<WordInfo> encodedData = hashMap::compressHashMap(INPUT);
+    /*std::vector<WordInfo> encodedData = HashMap::compressHashMap(INPUT);
 
 
     std::cout << "Encoded data: " << std::endl;
@@ -15,7 +16,7 @@ int main() {
         std::cout << entry.code << ":" << entry.distance << "%";
     }*/
 
-    BinaryTree<int> tree;
+    BinaryTree tree;
 
     std::ifstream inputFile("../public/input.dat", std::ios::binary);
     if (!inputFile) {
@@ -28,18 +29,47 @@ int main() {
         tree.insertNode(value);
     }
 
+    try {
+        BitReader reader("../public/input.bin");
+
+        while (reader.hasMoreBits()) {
+            uint32_t byte = reader.readBits(8);
+            auto ucharValue = static_cast<unsigned char>(byte);
+            tree.insertNode(ucharValue);
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
     inputFile.close();
 
-    std::cout << "Preorder traversal: ";
-    tree.preorder();
+    std::cout << "Postorder traversal: ";
+    tree.postorder();
 
-    tree.saveTreeToFile("../public/output.dat");
+    tree.saveTreeToFile("../public/output.bin");
 
-    BinaryTree<int> newTree;
-    newTree.loadTreeFromFile("../public/output.dat");
+    BinaryTree newTree;
+    try {
+        BitReader reader("../public/output.bin");
 
-    std::cout << "Preorder traversal: ";
-    newTree.preorder();
+        while (reader.hasMoreBits()) {
+            uint32_t byte = reader.readBits(8);
+            auto ucharValue = static_cast<unsigned char>(byte);
+            newTree.insertNode(ucharValue);
+        }
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    std::cout << "Postorder traversal: ";
+    newTree.postorder();
+
+
+    /*BinaryTree newTree;
+    newTree.loadTreeFromFile("../public/output.bin");
+
+    std::cout << "Postorder traversal: ";
+    tree.postorder();
 
     /*std::cout << "Inorder traversal: ";
     tree.inorder();
@@ -47,8 +77,7 @@ int main() {
     std::cout << "Preorder traversal: ";
     tree.preorder();
 
-    std::cout << "Postorder traversal: ";
-    tree.postorder();
+
 
     std::cout << "Level order traversal: ";
     tree.levelOrder();
@@ -58,7 +87,11 @@ int main() {
 
     tree.deleteNode(3);
     std::cout << "Inorder traversal after removing 3: ";
-    tree.inorder();*/
+    tree.inorder();
+
+    */
+
+
 
     return 0;
 }
