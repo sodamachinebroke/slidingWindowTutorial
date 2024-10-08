d2ed6ff8b975eabdcc406134302e40e097e92241
ersek <ersek.norbert@simplesoft.hu>
2024-10-08 18:59:42 +0200

    add constructor to Node.h

--- src/Node.h
+++ src/Node.h
@@ -3,16 +3,19 @@
 
 #ifndef NODE_H
 #define NODE_H
+
 #include <cstdint>
 
 
 class Node {
 public:
-    explicit Node(uint8_t value);
+
 
     Node *left;
     Node *right;
     uint8_t data;
+
+    explicit Node(uint8_t value) : data(value), left(nullptr), right(nullptr) {}
 };
 
 
