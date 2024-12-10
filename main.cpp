76483dd8585c1e69378a441dfb16e3e91c115603
ersek <ersek.norbert@simplesoft.hu>
2024-12-10 10:25:23 +0100

    modify code to accomodate binary data
    Signed-off-by: ersek <ersek.norbert@simplesoft.hu>

--- main.cpp
+++ main.cpp
@@ -1,159 +1,106 @@
-// #include "src/HuffmanCodec.h"
-//
-// int main() {
-//     HuffmanCodec codec;
-//     std::string input = "AAAABBBCCD";
-//
-//     // Build the codes
-//     codec.build(input);
-//     codec.printCodes();
-//
-//     // Encode to binary
-//     std::string encodedFile = "encoded.bin";
-//     codec.encode(input, encodedFile);
-//
-//     // Decode from binary
-//     std::string decoded;
-//     codec.decode(encodedFile, decoded);
-//
-//     std::cout << "Decoded: " << decoded << '\n';
-//
-//     return 0;
-// }
-
-// C++ program to encode and decode a string using
-// Huffman Coding.
 #include <bits/stdc++.h>
-#define MAX_TREE_HT 256
-using namespace std;
 
-// to map each character its huffman value
-map<char, string> codes;
+std::map<uint8_t, std::string> codes;
+std::map<uint8_t, int> freq;
 
-// To store the frequency of character of the input data
-map<char, int> freq;
-
-// A Huffman tree node
 struct MinHeapNode {
-	char data; // One of the input characters
-	int freq; // Frequency of the character
-	MinHeapNode *left, *right; // Left and right child
+    uint8_t data; // One of the input characters
+    int freq; // Frequency of the character
+    MinHeapNode *left, *right; // Left and right child
 
-	MinHeapNode(char data, int freq) {
-		left = right = NULL;
-		this->data = data;
-		this->freq = freq;
-	}
+    MinHeapNode(uint8_t data, int freq) {
+        left = right = NULL;
+        this->data = data;
+        this->freq = freq;
+    }
 };
 
-// utility function for the priority queue
 struct compare {
-	bool operator()(MinHeapNode *l, MinHeapNode *r) {
-		return (l->freq > r->freq);
-	}
+    bool operator()(MinHeapNode *l, MinHeapNode *r) {
+        return (l->freq > r->freq);
+    }
 };
 
-// utility function to print characters along with
-// there huffman value
-void printCodes(struct MinHeapNode *root, string str) {
-	if (!root)
-		return;
-	if (root->data != '$')
-		cout << root->data << ": " << str << "\n";
-	printCodes(root->left, str + "0");
-	printCodes(root->right, str + "1");
+void printCodes(struct MinHeapNode *root, std::string str) {
+    if (!root)
+        return;
+    if (root->data != 0)
+        std::cout << root->data << ": " << str << "\n";
+    printCodes(root->left, str + "0");
+    printCodes(root->right, str + "1");
 }
 
-// utility function to store characters along with
-// there huffman value in a hash table, here we
-// have C++ STL map
-void storeCodes(struct MinHeapNode *root, string str) {
-	if (root == NULL)
-		return;
-	if (root->data != '$')
-		codes[root->data] = str;
-	storeCodes(root->left, str + "0");
-	storeCodes(root->right, str + "1");
+void storeCodes(struct MinHeapNode *root, std::string str) {
+    if (root == NULL)
+        return;
+    if (root->data != 0)
+        codes[root->data] = str;
+    storeCodes(root->left, str + "0");
+    storeCodes(root->right, str + "1");
 }
 
-// STL priority queue to store heap tree, with respect
-// to their heap root node value
-priority_queue<MinHeapNode *, vector<MinHeapNode *>, compare>
-minHeap;
+std::priority_queue<MinHeapNode *, std::vector<MinHeapNode *>, compare> minHeap;
 
-// function to build the Huffman tree and store it
-// in minHeap
-void HuffmanCodes(int size) {
-	struct MinHeapNode *left, *right, *top;
-	for (map<char, int>::iterator v = freq.begin();
-	     v != freq.end(); v++)
-		minHeap.push(new MinHeapNode(v->first, v->second));
-	while (minHeap.size() != 1) {
-		left = minHeap.top();
-		minHeap.pop();
-		right = minHeap.top();
-		minHeap.pop();
-		top = new MinHeapNode('$',
-		                      left->freq + right->freq);
-		top->left = left;
-		top->right = right;
-		minHeap.push(top);
-	}
-	storeCodes(minHeap.top(), "");
+void HuffmanCodes() {
+    struct MinHeapNode *left, *right, *top;
+    for (auto &v: freq) minHeap.push(new MinHeapNode(v.first, v.second));
+
+    while (minHeap.size() != 1) {
+        left = minHeap.top();
+        minHeap.pop();
+        right = minHeap.top();
+        minHeap.pop();
+        top = new MinHeapNode(0, left->freq + right->freq);
+        top->left = left;
+        top->right = right;
+        minHeap.push(top);
+    }
+    storeCodes(minHeap.top(), "");
 }
 
-// utility function to store map each character with its
-// frequency in input string
-void calcFreq(string str, int n) {
-	for (int i = 0; i < str.size(); i++)
-		freq[str[i]]++;
+void calcFreq(const std::vector<uint8_t> &data) {
+    for (uint8_t byte: data) freq[byte]++;
 }
 
-// function iterates through the encoded string s
-// if s[i]=='1' then move to node->right
-// if s[i]=='0' then move to node->left
-// if leaf node append the node->data to our output string
-string decode_file(struct MinHeapNode *root, string s) {
-	string ans = "";
-	struct MinHeapNode *curr = root;
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] == '0')
-			curr = curr->left;
-		else
-			curr = curr->right;
+std::string decode_file(struct MinHeapNode *root, std::string s) {
+    std::string ans = "";
+    struct MinHeapNode *curr = root;
+    for (char bit: s) {
+        curr = (bit == '0') ? curr->left : curr->right;
 
-		// reached leaf node
-		if (curr->left == NULL and curr->right == NULL) {
-			ans += curr->data;
-			curr = root;
-		}
-	}
-	// cout<<ans<<endl;
-	return ans + '\0';
+        if (curr->left == nullptr && curr->right == nullptr) {
+            ans += curr->data;
+            curr = root;
+        }
+    }
+
+    return ans;
 }
 
-// Driver code
-int main() {
-	//TODO understand this. HOW DOES THIS WORK????? It supposedly makes a Huffman Tree out of something, but aaaaaaaaaaaaaaaaa
-	string str = "AAAABBBCCD";
-	string encodedString, decodedString;
-	calcFreq(str, str.length());
-	HuffmanCodes(str.length());
-	cout << "Character With there Frequencies:\n";
-	for (auto v = codes.begin(); v != codes.end(); v++)
-		cout << v->first << ' ' << v->second << endl;
+int main() {//currently works on uint8_t, now just need to integrate file reading and writing
+    //TODO understand this. HOW DOES THIS WORK????? It supposedly makes a Huffman Tree out of something, but aaaaaaaaaaaaaaaaa
+    std::vector<uint8_t> data = {65, 65, 65, 65, 66, 66, 66, 67, 67, 68};
+    std::string encodedString, decodedString;
 
-	for (auto i: str)
-		encodedString += codes[i];
+    calcFreq(data);
+    HuffmanCodes();
+    std::cout << "Character With there Frequencies:\n";
+    for (auto &v: freq) std::cout << (int) v.first << ": " << v.second << std::endl;
 
-	cout << "\nEncoded Huffman data:\n"
-			<< encodedString << endl;
+    std::cout << std::endl << "Huffman Codes:" << std::endl;
 
-	// Function call
-	decodedString
-			= decode_file(minHeap.top(), encodedString);
-	cout << "\nDecoded Huffman Data:\n"
-			<< decodedString << endl;
-	return 0;
+    for (auto &v: codes) std::cout << (int) v.first << ": " << v.second << std::endl;
+    for (uint8_t byte: data)
+        encodedString += codes[byte];
+
+    std::cout << std::endl << "Encoded Huffman data:" << std::endl << encodedString << std::endl;
+
+    // Function call
+    decodedString = decode_file(minHeap.top(), encodedString);
+    std::cout << "\nDecoded Huffman Data:\n" << decodedString << std::endl;
+
+    for (char byte: decodedString) std::cout << (int) (uint8_t) byte << ' ';
+    std::cout << std::endl;
+    return 0;
 }
 
