8705b937e1485be0c76c8889f98fad499608e0a7
ersek <ersek@simplesoft.ss>
2024-12-11 13:22:01 +0100

    clean up code

--- main.cpp
+++ main.cpp
@@ -5,15 +5,15 @@ struct MinHeapNode {
     int freq; // Frequency of the character
     MinHeapNode *left, *right; // Left and right child
 
-    MinHeapNode(uint8_t data, int freq) {
-        left = right = NULL;
+    MinHeapNode(const uint8_t data, const int freq) {
+        left = right = nullptr;
         this->data = data;
         this->freq = freq;
     }
 };
 
 struct compare {
-    bool operator()(MinHeapNode *l, MinHeapNode *r) {
+    bool operator()(const MinHeapNode *l, const MinHeapNode *r) const {
         return (l->freq > r->freq);
     }
 };
@@ -22,7 +22,7 @@ std::map<uint8_t, std::string> codes;
 std::map<uint8_t, int> freq;
 std::priority_queue<MinHeapNode *, std::vector<MinHeapNode *>, compare> minHeap;
 
-void printCodes(struct MinHeapNode *root, std::string str) {
+void printCodes(const struct MinHeapNode *root, const std::string &str) {
     if (!root)
         return;
     if (root->data != 0)
@@ -31,8 +31,8 @@ void printCodes(struct MinHeapNode *root, std::string str) {
     printCodes(root->right, str + "1");
 }
 
-void storeCodes(struct MinHeapNode *root, std::string str) {
-    if (root == NULL)
+void storeCodes(const struct MinHeapNode *root, const std::string &str) {
+    if (root == nullptr)
         return;
     if (root->data != 0)
         codes[root->data] = str;
@@ -62,10 +62,10 @@ void calcFreq(const std::vector<uint8_t> &data) {
     for (uint8_t byte: data) freq[byte]++;
 }
 
-std::string decode_file(struct MinHeapNode *root, std::string s) {
-    std::string ans = "";
-    struct MinHeapNode *curr = root;
-    for (char bit: s) {
+std::string decode_file(const struct MinHeapNode *root, const std::string &s) {
+    std::string ans;
+    const struct MinHeapNode *curr = root;
+    for (const char bit: s) {
         curr = (bit == '0') ? curr->left : curr->right;
 
         if (curr->left == nullptr && curr->right == nullptr) {
@@ -77,29 +77,30 @@ std::string decode_file(struct MinHeapNode *root, std::string s) {
     return ans;
 }
 
-int main() {//currently works on uint8_t, now just need to integrate file reading and writing
+int main() {
+    //currently works on uint8_t, now just need to integrate file reading and writing
     //TODO understand this. HOW DOES THIS WORK????? It supposedly makes a Huffman Tree out of something, but aaaaaaaaaaaaaaaaa
     std::vector<uint8_t> data = {65, 65, 65, 65, 66, 66, 66, 67, 67, 68};
-    std::string encodedString, decodedString;
+    std::string encodedString;
 
     calcFreq(data);
     HuffmanCodes();
     std::cout << "Character With there Frequencies:\n";
-    for (auto &v: freq) std::cout << (int) v.first << ": " << v.second << std::endl;
+    for (auto &v: freq) std::cout << static_cast<int>(v.first) << ": " << v.second << std::endl;
 
     std::cout << std::endl << "Huffman Codes:" << std::endl;
 
-    for (auto &v: codes) std::cout << (int) v.first << ": " << v.second << std::endl;
+    for (auto &v: codes) std::cout << static_cast<int>(v.first) << ": " << v.second << std::endl;
     for (uint8_t byte: data)
         encodedString += codes[byte];
 
     std::cout << std::endl << "Encoded Huffman data:" << std::endl << encodedString << std::endl;
 
     // Function call
-    decodedString = decode_file(minHeap.top(), encodedString);
+    std::string decodedString = decode_file(minHeap.top(), encodedString);
     std::cout << "\nDecoded Huffman Data:\n" << decodedString << std::endl;
 
-    for (char byte: decodedString) std::cout << (int) (uint8_t) byte << ' ';
+    for (char byte: decodedString) std::cout << static_cast<int>(static_cast<uint8_t>(byte)) << ' ';
     std::cout << std::endl;
     return 0;
 }
